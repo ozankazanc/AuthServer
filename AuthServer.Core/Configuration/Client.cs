0152f45259097b49ed433c5c3da193eb162e82db
Ozan Kazanç <ozan.kazanc@hotmail.com>
2022-03-13 13:43:02 +0300

    modified core layer

---
+++ AuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Configuration
+{
+    public class Client
+    {
+        public string Id { get; set; }
+        public string Secret { get; set; }
+        /// <summary>
+        /// izleyiciler
+        /// www.api.com , www.api2.com
+        /// erişim yapabileceği yollar tutulur.
+        /// </summary>
+        public List<string> Audiences { get; set; } 
+    }
+}
---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,23 @@
+﻿using AuthServer.Core.Dtos;
+using AuthServer.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<Response<TokenDto>> CreateToken(LoginDto loginDto);
+        Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+        /// <summary>
+        /// refresh token'ı temizlememize olanak tanıyacak.
+        /// </summary>
+        /// <param name="refreshToken"></param>
+        /// <returns></returns>
+        Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+        Task<Response<ClientLoginDto>> CreateTokenByClient(ClientTokenDto clientTokenDto);
+    }
+}
