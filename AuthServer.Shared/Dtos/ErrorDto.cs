f2c67691cebb9333b8a929bb15952f49e1dd9adc
Ozan Kazanç <ozan.kazanc@hotmail.com>
2022-03-13 12:42:23 +0300

    Added Error, Response, TokenDtos

---
+++ AuthServer.Shared/Dtos/ErrorDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Shared.Dtos
+{
+    public class ErrorDto
+    {
+        public List<string> Errors { get; private set; }
+        public bool IsShow { get; private set; }
+        //Yazılım tarafında oluşan hataları sadece Developer'in görmesi için bu property oluşturuldu.
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+        public ErrorDto(string error,bool isShow)
+        {
+            Errors.Add(error);
+            isShow = true;
+        }
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            Errors = errors;
+            IsShow = isShow;
+        }
+    }
+}
---
+++ AuthServer.Shared/Dtos/Response.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AuthServer.Shared.Dtos
+{
+    public class Response<T> where T : class
+    {
+        public T Data { get; set; }
+        public int StatusCode { get; set; }
+        public ErrorDto Error { get; set; }
+
+        [JsonIgnore] // success bilgisini dışarıya vermiyoruz.
+        public bool IsSuccess { get; set; }
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccess = true };
+        }
+        public static Response<T> Success(int statusCode)
+        {
+            return new Response<T> { Data = default, StatusCode = statusCode, IsSuccess = true };
+        }
+        public static Response<T> Fail(ErrorDto errorDto, int statusCode)
+        {
+            return new Response<T> { Error = errorDto, StatusCode = statusCode, IsSuccess = false };
+        }
+        public static Response<T> Fail(string errorMessage, int statusCode, bool isShow)
+        {
+            var errorDto = new ErrorDto(errorMessage, isShow);
+            return new Response<T> { Error = errorDto, StatusCode = statusCode, IsSuccess = false };
+        }
+    }
+}
