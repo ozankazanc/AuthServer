6997e0a8ad90c93c2f6b5051c5a2a063bdaa2f87
Ozan Kazanç <ozan.kazanc@hotmail.com>
2022-03-13 11:27:12 +0300

    Core -> added repository

---
+++ AuthServer.Core/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthServer.Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string UserId { get; set; }
+    }
+}
